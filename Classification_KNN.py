dbbf5aa794fc24ff3dfca6da5de240edbf30f468
Jas000n <77543568+Jas000n@users.noreply.github.com>
2022-06-11 19:05:54 +0800

    torchNMF sklearnNMF surpriseNMF myNMF_singleThread myNMF_multiThread surprise_k_top my_k_top_300(from 0-943 instead of 0-942) everything done for distribution can classify user into 2 groups

---
+++ Classification_KNN.py
@@ -0,0 +1,48 @@
+import numpy as np
+from sklearn.cluster import KMeans
+
+import mv100
+from scipy.sparse import csr_matrix
+train_list = mv100.mv1002list("./ml-100k/u5_fix.base")
+# test_list = mv100.mv1002list("./ml-100k/u5.test")
+# test_rm = mv100.creat_matrix(test_list)
+train_rm = mv100.creat_matrix(train_list)
+print("in the movie-100k datasets, there are {} users and {} items !".format(train_rm.shape[0], train_rm.shape[1]))
+
+print(train_rm)
+kmeans = KMeans(n_clusters=2, random_state=0,max_iter=100000,tol=1e-10).fit(train_rm)
+labels = kmeans.labels_
+type1=[]
+type2=[]
+for i in range(labels.shape[0]):
+    if(labels[i]==0):
+        type1.append(i)
+    else:
+        type2.append(i)
+type1_rates=[[1,0],[2,0],[3,0],[4,0],[5,0]]
+type2_rates=[[1,0],[2,0],[3,0],[4,0],[5,0]]
+print('type1=',type1)
+print('type2=',type2)
+sum1=0
+sum2=0
+for i in train_list:
+    if(i[0] in type1):
+        j = int(i[2])-1
+        type1_rates[j][1]+=1
+        sum1+=1
+    else:
+        j = int(i[2])-1
+        type2_rates[j][1] += 1
+        sum2+=1
+for i in type1_rates:
+    i[1]=i[1]/sum1*100
+for i in type2_rates:
+    i[1]=i[1]/sum2*100
+type1_rates_ndarray = np.array(type1_rates)
+type2_rates_ndarray = np.array(type2_rates)
+
+np.save("./clusters/type1",type1_rates_ndarray)
+np.save("./clusters/type2",type2_rates_ndarray)
+print(type1_rates_ndarray)
+print(type2_rates_ndarray)
+print("done!")
--- distribution.py
+++ distribution.py
@@ -5,35 +5,36 @@ from surprise import NMF
 from surprise import Dataset
 
 import mv100
-mode = "random"
-if mode =="general_mymodel":
+
+mode = "clustered"
+if mode == "general_mymodel":
     erm = np.load("save_matrix299.npy", allow_pickle=True)
-    erm  = erm.reshape((1, -1))
+    erm = erm.reshape((1, -1))
 
     rm = mv100.mv1002list("./ml-100k/u5_fix.base")
-    rm = np.array(rm,dtype=int)
-    rm = rm[:,2]
-    sns.distplot(rm,label="actual distribution")
+    rm = np.array(rm, dtype=int)
+    rm = rm[:, 2]
+    sns.distplot(rm, label="actual distribution")
     sns.distplot(erm, label="estimated distribution")
     plt.title("my model")
     plt.legend()
     plt.show()
-elif mode =="random":
-    while(True):
+elif mode == "random":
+    while (True):
         u = eval(input("check density distribution on some random user:"))
         erm_sup = np.load("sup_model.npy", allow_pickle=True)
-        erm_sup = erm_sup[u,:]
+        erm_sup = erm_sup[u, :]
         erm_sup = erm_sup.reshape((1, -1))
         erm_mine = np.load("save_matrix299.npy", allow_pickle=True)
-        erm_mine = erm_mine[u,:]
+        erm_mine = erm_mine[u, :]
         erm_mine = erm_mine.reshape((1, -1))
         rm = mv100.mv1002list("./ml-100k/u5_fix.base")
         rm = np.array(rm, dtype=int)
         U_rm = []
         for i in rm:
-            if(i[0]==u):
+            if (i[0] == u):
                 U_rm.append(i[2])
-        #print(len(U_rm))
+        # print(len(U_rm))
         sns.distplot(erm_sup, label="estimated distribution from SUPRISE")
         sns.distplot(erm_mine, label="estimated distribution from My_Model")
         sns.distplot(U_rm, label="actual distribution")
@@ -52,7 +53,7 @@ elif mode == "general_sup":
     plt.title("Surprise NMF library")
     plt.legend()
     plt.show()
-elif mode =="general_compare":
+elif mode == "general_compare":
     erm_sup = np.load("sup_model.npy", allow_pickle=True)
     erm_sup = erm_sup.reshape((1, -1))
     erm_mine = np.load("save_matrix299.npy", allow_pickle=True)
@@ -65,5 +66,14 @@ elif mode =="general_compare":
     sns.distplot(erm_sup, label="estimated distribution_sup")
     sns.distplot(erm_mine, label="estimated distribution_mine")
     plt.title("Surprise vs mine")
+    plt.legend()
+    plt.show()
+elif mode == "clustered":
+    type1 = np.load("./clusters/type1.npy", allow_pickle=True)
+    type2 = np.load("./clusters/type2.npy", allow_pickle=True)
+
+    sns.distplot(type1, label="preferences of type1 people")
+    sns.distplot(type2, label="preferences of type2 people")
+    plt.title("my model")
     plt.legend()
     plt.show()
